1fa9a14443ba1afdb9d59fcbc2ffb402a46c9f6f
nikki-infinte <139063803+nikki-infinte@users.noreply.github.com>
2024-08-13 19:01:42 +0530

    Time: 760 ms (5.10%) | Memory: 152.6 MB (7.05%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,49 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>>output;
-    bool isValid(int n,int m,int i,int j,vector<vector<int>>& land)
+    int n,m;
+    vector<vector<int>>result;
+    vector<vector<int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
+    void dfs(vector<vector<int>>& land,int i,int j,int &r2,int &c2)
     {
-           if (i >= 0 && i < n && j >= 0 && j < m && land[i][j] == 1) {
-                return true;
-            }
-            else{
-                return false;
-            }
-    }
-    void DFS(int n,int m,int i,int j,vector<vector<int>>& land,int& minRow, int& maxRow, int& minCol, int& maxCol)
-    {
+        land[i][j]=0;
+        r2=max(r2,i);
+        c2=max(c2,j);
 
-        minRow = min(minRow, i);
-        maxRow = max(maxRow, i);
-        minCol = min(minCol, j);
-        maxCol = max(maxCol, j);
-        
-        land[i][j] = 0; // Mark cell as visited
-        
-        // Explore neighboring cells
-        if (isValid(n, m, i + 1, j, land)) DFS(n, m, i + 1, j, land, minRow, maxRow, minCol, maxCol);
-        if (isValid(n, m, i - 1, j, land)) DFS(n, m, i - 1, j, land, minRow, maxRow, minCol, maxCol);
-        if (isValid(n, m, i, j + 1, land)) DFS(n, m, i, j + 1, land, minRow, maxRow, minCol, maxCol);
-        if (isValid(n, m, i, j - 1, land)) DFS(n, m, i, j - 1, land, minRow, maxRow, minCol, maxCol);
+        for(auto dir:directions)
+        {
+            int new_i=i+dir[0];
+            int new_j=j+dir[1];
+
+            if(new_i >=0 && new_i <n && new_j >=0 && new_j <m &&land[new_i][new_j]==1)
+            {
+                dfs(land,new_i,new_j,r2,c2);
+            }
+        }
     }
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        int n=land.size();
-        int m=land[0].size();
-
-    
+      
+        n=land.size();
+        m=land[0].size();
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(land[i][j]==1)
                 {
-                     int minRow = i, maxRow = i, minCol = j, maxCol = j;
-                     DFS(n, m, i, j, land, minRow, maxRow, minCol, maxCol);
-                    output.push_back({minRow, minCol, maxRow, maxCol});
-
+                    // cor.push_back(i);
+                    // cor.push_back(j);
+                    int r1=i;
+                    int c1=j;
+                    int r2=i;
+                    int c2=j;
+                    dfs(land,i,j,r2,c2);
+                    result.push_back({r1,c1,r2,c2});
                 }
             }
         }
-        return output;
+          return result;
     }
+  
 };
