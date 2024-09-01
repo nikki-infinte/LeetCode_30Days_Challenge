187e27146d4140947c5b2236ff27a3379fb5ab05
nikki-infinte <139063803+nikki-infinte@users.noreply.github.com>
2024-09-02 00:34:49 +0530

    Time: 98 ms (8.92%) | Memory: 93 MB (16.04%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(m*n !=original.size())
+        {
+            return {};
+         }
+
+         std::vector<std::vector<int>> result(m, std::vector<int>(n));
+
+        for (int i = 0; i < m; ++i) {
+            result[i] = std::vector<int>(original.begin() + i * n, original.begin() + i * n + n);
+        }
+
+        return result;
+    }
+};
