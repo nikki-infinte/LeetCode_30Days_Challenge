6c263dde9db6fd9c4cac178cb50d1c0d4d8d84b8
nikki-infinte <139063803+nikki-infinte@users.noreply.github.com>
2024-09-17 23:12:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (45.08%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    map<string,int>mp;
+    void stringstreamm(string s)
+    {
+        stringstream ss(s);
+        string word;
+        while(ss>>word)
+        {
+            mp[word]++;
+        }
+    }
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        stringstreamm(s1);
+        stringstreamm(s2);
+
+        vector<string>ans;
+        for(auto x:mp)
+        {
+            if(x.second==1)
+            {
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
