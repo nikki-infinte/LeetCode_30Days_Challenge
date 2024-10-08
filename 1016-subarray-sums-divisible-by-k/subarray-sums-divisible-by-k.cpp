a332555d80b4dd1a261e0668a3c35d5bc484ce57
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2024-10-08 17:42:12 +0530

    Time: 46 ms (36.46%) | Memory: 35.5 MB (58.01%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -1,26 +1,30 @@
-class Solution
-{
+class Solution {
 public:
-    int subarraysDivByK(vector<int> &nums, int k)
-    {
-        // Array to store the frequency of remainders
-        int modulusCount[k];
-        // Initialize the remainders array with 0
-        memset(modulusCount, 0, sizeof(modulusCount));
-        int currentMod = 0; // Current prefix sum remainder
-        int subarrayCount = 0;    // Count of subarrays divisible by k
-        modulusCount[0] = 1;    // There is one way to have a remainder of 0 (empty subarray)
-        // Iterate through the array
-        for (int i = 0; i < nums.size(); i++)
+    int subarraysDivByK(vector<int>& nums, int k) {
+        
+        //using optimal o(n) approach
+
+        unordered_map<int,int>umap;
+        umap[0]=1;
+        int res=0;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
         {
-            // Update the current remainder by adding the current number and taking modulo k
-            // Adding k before taking modulo k ensures we handle negative numbers correctly
-            currentMod = ((currentMod + nums[i] % k) + k) % k;
-            // Add the number of times this remainder has occurred before to the count
-            subarrayCount += modulusCount[currentMod];
-            // Increment the frequency of the current remainder
-            modulusCount[currentMod]++;
+            sum+=nums[i];
+            int rem=sum%k;
+            if(rem <0)
+            {
+                rem+=k;
+            }
+
+            if(umap.find(rem)!=umap.end())
+            {
+                res+=umap[rem];
+                
+            }
+            umap[rem]++;
+
         }
-        return subarrayCount;
+        return res;
     }
 };
