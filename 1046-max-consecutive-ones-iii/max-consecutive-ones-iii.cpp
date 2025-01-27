74f482a24ef64e0f1702dcdce95572620ef2e2ce
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-01-27 18:09:03 +0530

    Time: 7 ms (8.42%) | Memory: 59.4 MB (26.03%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,29 +1,74 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int i=0;
-        int j=0;
-        int sum=0;
-        int maxone=0;
-        while(j<nums.size())
+        
+        int zero= accumulate(nums.begin(),nums.end(),0);
+        if(k==0 && zero==0)
         {
-           //sum
-           sum+=nums[j];
-           if( (j-i+1)-sum==k  || ((j-i+1)-sum <=k ))
-           {
-            maxone=max(maxone,(j-i+1));
-          
-           }
-           if ((j - i + 1) - sum > k) {
-            // Shrink the window from the left
-            if (nums[i] == 1) {
-                --sum;
-            }
-            ++i;
-           }
-           j++;
+            return 0;
         }
+        // //two pointer apprach
+        // int n=nums.size();
+        // int power = k;
+        // int i=0;int j=0;
+        // int maxlen=0;
 
-        return maxone;
+        // int sum=0;
+        // while(j<n)
+        // {
+        //     if(nums[j]==0 && power > 0)
+        //     {
+        //         power--;
+        //     }
+        //     sum+=nums[j];
+
+        //     if(power==0)
+        //     {
+        //          maxlen=max(maxlen,j-i+1);
+        //         while( power < k && i < j)
+        //         {
+        //             if(nums[i]==0){
+        //                 power++;
+        //             }
+        //             sum=sum-nums[i];
+        //             i++;
+        //         }
+                 
+        //     }
+          
+        //     j++;
+
+        // }
+        // return maxlen;
+
+        int zerocnt=0;
+        int i=0,j=0;
+        int maxlen=INT_MIN;
+        int n=nums.size();
+
+        while( j < n )
+        {
+            if(nums[j] == 0)
+            {
+                zerocnt++;
+            }
+
+            if(zerocnt <= k)
+            {
+                maxlen=max(maxlen , j-i+1);
+            }else{
+                while(zerocnt > k && i<j)
+                {
+                    if(nums[i]==0)
+                    {
+                        zerocnt--;
+                        
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return maxlen;
     }
 };
