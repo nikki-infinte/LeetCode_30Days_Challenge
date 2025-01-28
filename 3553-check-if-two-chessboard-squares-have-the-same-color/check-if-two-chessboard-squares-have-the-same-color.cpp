1ccd8544ed3eda4a64558d0304309309d4da6def
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-01-28 19:13:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (83.64%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.cpp
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.cpp
@@ -1,34 +1,8 @@
 class Solution {
 public:
-    bool checkTwoChessboards(string coordinate1, string coordinate2) {
-       
-        int chara=coordinate1[0]-'a'+1;
-        int value=coordinate1[1];
+    bool checkTwoChessboards(string c1, string c2) {
+        
 
-        int chara1 = coordinate2[0]-'a'+1;
-        int value1=coordinate2[1];
-
-        if( (chara % 2 && chara1 % 2 ) || (chara%2==0 && chara1 % 2 ==0) )
-        {
-            if( value % 2 == 0 && value1 % 2 ==0 )
-            {
-                return true;
-            }else if( (value % 2 != 0 && value1 % 2 !=0 ) )
-            {
-                return true;
-            }else{
-                return false;
-            }
-        }else {
-             if( value % 2 == 0 && value1 % 2 ==0 )
-            {
-                return false;
-            }else if( (value % 2 != 0 && value1 % 2 !=0 ) )
-            {
-                return false;
-            }else{
-                return true;
-            }
-        }
+        return ( (c1[0]+c1[1])%2 == (c2[0]+c2[1])%2 );
     }
 };
