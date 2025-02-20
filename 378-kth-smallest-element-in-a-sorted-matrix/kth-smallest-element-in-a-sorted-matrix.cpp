24fd2aa24b267c8588286c07bd589e2e50b56918
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-02-20 16:41:06 +0530

    Time: 10 ms (31.70%) | Memory: 17.1 MB (69.82%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& mat, int k) {
-        priority_queue<int>min_heap;
-        int n=mat.size();
-  for(int i=0;i < n;i++)
-  {
-      for(int j=0;j<n;j++)
-      {
-         min_heap.push(mat[i][j]);
-         while(min_heap.size() > k)
-         {
-             min_heap.pop();
-         }
-      }
-  }
-  
-  return min_heap.top();
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+
+         int n = matrix.size();
+        
+        // Min-heap to store the {value, {row, col}}
+        priority_queue<pair<int, pair<int, int>>, 
+                       vector<pair<int, pair<int, int>>>, 
+                       greater<pair<int, pair<int, int>>>> p;
+
+        // Push the first column of each row into the min-heap
+        for (int i = 0; i < n; i++) {
+            p.push({matrix[i][0], {i, 0}});
+        }
+        int ans=0;
+        while (k--) {
+            auto element = p.top();
+            p.pop();
+            ans = element.first;
+            int i = element.second.first;
+            int j = element.second.second;
+
+            // Push the next column element of the same row if exists
+            if (j + 1 < n) {
+                p.push({matrix[i][j + 1], {i, j + 1}});
+            }
+        }
+        return ans;
+
     }
 };
