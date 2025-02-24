9446b99441d110c5c380472d7d54468254a18e77
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-02-24 21:09:37 +0530

    Time: 1 ms (35.65%) | Memory: 11.3 MB (20.50%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> ans;
+        unordered_map<int, int> umap;
         
-        map<int,int> mp;
-        for(auto m:arr1)
-        {
-            mp[m]++;
+        // Count occurrences of elements in arr1
+        for (int num : arr1) {
+            umap[num]++;
         }
 
-        vector<int>ans;
-        for(int i=0;i<arr2.size();i++)
-        {
-            if(mp.find(arr2[i])!=mp.end())
-            {
-                int count=mp[arr2[i]];
-                while(count>0)
-                {
-                    ans.push_back(arr2[i]);
-                    count--;
-                }
-                if(count==0)
-                {
-                    mp.erase(arr2[i]);
-                }
+        // Add elements that are in arr2 in the correct order
+        for (int num : arr2) {
+            while (umap[num] > 0) {
+                ans.push_back(num);
+                umap[num]--;
             }
+            umap.erase(num); // Remove processed element from map
+        }
 
-        }
-        for(auto &pair:mp)
-        {
-            for(int i=0;i<pair.second;i++)
-            {
-                ans.push_back(pair.first);
+        // Store remaining elements that were not in arr2
+        vector<int> remaining;
+        for (auto& [num, count] : umap) {
+            while (count-- > 0) {
+                remaining.push_back(num);
             }
         }
+
+        // Sort the remaining elements
+        sort(remaining.begin(), remaining.end());
+
+        // Append remaining elements to ans
+        ans.insert(ans.end(), remaining.begin(), remaining.end());
 
         return ans;
     }
