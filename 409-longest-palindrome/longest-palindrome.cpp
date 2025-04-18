74f9fa3015b714c52f41acc400e4cc42765bea86
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-04-18 11:19:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (62.54%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int oddFrequencyCount = 0;
-        unordered_map<char, int> charFrequency;
-        for (char ch : s) {
-            charFrequency[ch]++;
-            if (charFrequency[ch] % 2 == 1)
-                oddFrequencyCount++;
-            else
-                oddFrequencyCount--;
+        unordered_map<char,int>umap;
+
+        for(auto a:s)
+        {
+            umap[a]++;
         }
-        if (oddFrequencyCount > 1)
-            return s.length() - oddFrequencyCount + 1;
-        return s.length();
+        int even=0 ;
+        bool odd=false;
+   
+        for(auto a:umap)
+        {
+            if(a.second % 2 ==0){
+                even+= a.second;
+                
+            }else{
+                even +=(a.second -1);
+                odd = true;
+            }
+        }
+        if(odd)
+        {
+            even++;
+        }
+
+    return even;
+        
     }
 };
