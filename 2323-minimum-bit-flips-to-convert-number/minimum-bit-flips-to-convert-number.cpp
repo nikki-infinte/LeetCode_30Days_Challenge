46b32f8c51964293711d8d2dc805e39f9a9b0d17
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-04-19 22:05:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (25.35%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    string bits(int num)
-    {
-        string ans="";
-        for(int i=31 ;i>=0;i--)
-        {
-            ans+=to_string(( num >> i)& 1);
-        }
-        return ans;
-    }
     int minBitFlips(int start, int goal) {
-        string res1 = bits(start);
-        string res2 = bits(goal);
-
-        int cnt =0;
-        for(int i=0;i<32;i++)
+        
+        int num = start ^ goal;
+        int cnt=0;
+        for(int i=31;i>=0;i--)
         {
-            if(res1[i] !=res2[i])
-            {
-                cnt++;
-            }
+            int ith_bit = (num>>i)&1;
+            if(ith_bit==1)cnt++;
         }
         return cnt;
     }
