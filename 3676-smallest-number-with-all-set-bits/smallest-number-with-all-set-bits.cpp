09769b2e24dbf7d770b13b4c7f53c0eb8d8d56ad
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-04-19 22:27:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (66.05%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-    
+        int mb_position = 0;
+        int x =n;
+        while( x >0 )
+        {
+            mb_position++;
+           x = x >>1;
+        }
 
-       int res=0;
-       for(int i=0;res<n;i++)
-       {
-        res=(res << 1)|1;
-       }
-       return res;
+        // now number of mb_position i want number of set bits 
+        return (1 << mb_position) - 1;
     }
 };
