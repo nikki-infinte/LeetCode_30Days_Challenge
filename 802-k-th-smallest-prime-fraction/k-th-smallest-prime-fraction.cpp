4988cedb18d9e8a5912fc87c8537e6fcd3b36c69
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-04-21 23:52:49 +0530

    Time: 213 ms (66.33%) | Memory: 12.6 MB (68.63%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
+    typedef pair<float, pair<int, int>> P;
+
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
         int n = arr.size();
-        priority_queue<pair<double,pair<int,int>>> pq;
+        priority_queue<P, vector<P>, greater<P>> min_heap;
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                double x = arr[i]/(arr[j]*1.0);
-                
-                if(pq.size() == k){
-                    if(x < pq.top().first){
-                        pq.pop();
-                        pq.push({x,{arr[i],arr[j]}});
-                    }
-                }
-                else{
-                    pq.push({x,{arr[i],arr[j]}});
-                }
+        // Push initial fractions arr[0]/arr[i] for i = 1 to n-1
+        for (int i = 1; i < n; i++) {
+            float frac = float(arr[0]) / arr[i];
+            min_heap.push({frac, {0, i}});
+        }
+
+        P top_pair;
+        while (k-- > 0) {
+            top_pair = min_heap.top();
+            min_heap.pop();
+
+            int i = top_pair.second.first;
+            int j = top_pair.second.second;
+
+            // Try moving to the next numerator
+            if (i + 1 < j) {
+                float new_frac = float(arr[i + 1]) / arr[j];
+                min_heap.push({new_frac, {i + 1, j}});
             }
         }
 
-        return {pq.top().second.first,pq.top().second.second};
+        // Get final answer
+        int i = top_pair.second.first;
+        int j = top_pair.second.second;
+        return {arr[i], arr[j]};
     }
 };
