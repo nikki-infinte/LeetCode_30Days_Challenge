9bb8590262b8421ab95012122c833d915efb8f23
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-04-23 21:13:08 +0530

    Time: 152 ms (7.50%) | Memory: 138.1 MB (5.28%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        //brute force
-        sort(nums.begin(),nums.end());
-
-        int maxdiff = 0;
-        int n = nums.size();
-        for(int i=0;i<n-1;i++)
-        {
-            maxdiff = max(maxdiff , abs(nums[i] - nums[i+1]) );
+        if (nums.size() < 2) return 0;
+        int hi = 0, lo = INT_MAX, ans = 0;
+        for (auto& n : nums)
+            hi = max(hi, n), lo = min(lo, n);
+        int bsize = max(int((hi - lo) / (nums.size() - 1)), 1);
+        vector<vector<int>> buckets((hi - lo) / bsize + 1, vector<int>());
+        for (auto& n : nums)
+            buckets[(n - lo) / bsize].push_back(n);
+        int currhi = 0;
+        for (auto& b : buckets) {
+            if (b.empty()) continue;
+            int prevhi = currhi ? currhi : b[0], currlo = b[0];
+            for (auto& n : b)
+                currhi = max(currhi, n), currlo = min(currlo, n);
+            ans = max(ans, currlo - prevhi);
         }
-        return maxdiff;
+        return ans;
     }
 };
