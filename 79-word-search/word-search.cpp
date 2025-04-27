67f8679e98dffe80495ead05dad9d72cb382c9ae
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-04-27 18:11:18 +0530

    Time: 238 ms (69.60%) | Memory: 10.8 MB (62.83%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-int n,m;
-    vector<vector<int>>direction={{1,0},{-1,0},{0,1},{0,-1}};
-    bool searchWord(vector<vector<char>>& board,int i,int j,int inx,string& word)
-    {
-        if(inx==word.size())
-        {
-            return true;
-        }
+    bool rec(int i, int j, int m, int n, int ind, vector<vector<char>>& board, string& word, vector<vector<bool>>& visited) {
+        if (ind == word.size()) return true;
 
-        if(i<0 || j<0 || i >= n|| j >= m ||board[i][j]=='$' || board[i][j] !=word[inx])
-        {
+        if (i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || board[i][j] != word[ind]) 
             return false;
-        }
 
-        char temp=board[i][j];
-        board[i][j]='$';
+        visited[i][j] = true;
 
-        for(auto& d:direction)
-        {
-            int new_i=i+d[0];
-            int new_j=j+d[1];
-        
-        if(searchWord(board,new_i,new_j,inx+1,word)){
-            return true;
-        }
-        }
-        board[i][j]=temp;
-        return false;
+        bool down = rec(i + 1, j, m, n, ind + 1, board, word, visited);
+        bool up = rec(i - 1, j, m, n, ind + 1, board, word, visited);
+        bool left = rec(i, j - 1, m, n, ind + 1, board, word, visited);
+        bool right = rec(i, j + 1, m, n, ind + 1, board, word, visited);
+
+        visited[i][j] = false; // Backtrack
+
+        return down || up || left || right;
     }
-    bool exist(vector<vector<char>>& board, string word) {
-         n=board.size();
-         m=board[0].size();
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(board[i][j]==word[0] && searchWord(board,i,j,0,word))
-                {
-                    return true;
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        
+        // Initialize visited matrix
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    if (rec(i, j, m, n, 0, board, word, visited)) 
+                        return true;
                 }
             }
         }
+
         return false;
     }
 };
