e3e46220ba5d39386e252c77af5491ee235d0065
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-04-27 20:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.78%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-         
-         int midx= m-1;
-         int nidx = n-1;
-         int right = m+n-1;
-
-         while( nidx >=0 )
-         {
-            if (midx >= 0 && nums1[midx] > nums2[nidx])
-            {
-                nums1[right] = nums1[midx];
-                midx --;
-            }else {
-                nums1[right] = nums2[nidx];
-                nidx --;
-            }
-            right --;
-         }
-      
         
+        int a = m-1,b = n -1;
+
+        int write_indx = m+n-1;
+      while (a >= 0 && b >= 0) {
+            if (nums1[a] > nums2[b]) {
+                nums1[write_indx--] = nums1[a--];
+            } else {
+                nums1[write_indx--] = nums2[b--];
+            }
+        }
+        while(b >=0)
+        {
+            nums1[write_indx--] =nums2[b--];
+        }
     }
 };
