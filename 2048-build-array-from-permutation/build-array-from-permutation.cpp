f6bca8e2faff85c654c97ef8fa790de9d2edc1c8
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-05-07 01:39:17 +0530

    Time: 2 ms (17.09%) | Memory: 20.5 MB (39.43%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int>ans;
+      
+      int  n =nums.size();
+      for(int i=0;i<n;i++)
+      {
+        int newval = nums[nums[i]]%n;
+        nums[i] +=newval*n;
+      }
 
-        for(int i=0;i<nums.size();i++)
-        {
-            ans.push_back(nums[nums[i]]);
-        }
-        return ans;
+      for(int i=0;i<n;i++)
+      {
+        nums[i]/=n;
+      }
+      return nums;
     }
 };
