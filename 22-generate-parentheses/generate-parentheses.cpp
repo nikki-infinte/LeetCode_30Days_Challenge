20420f64f75fecd1754f7d3ddb8290ef3752bcbb
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-05-10 16:33:50 +0530

    Time: 3 ms (70.89%) | Memory: 15.7 MB (49.99%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
 
-   void generate_pranthese(int open,int close,string s,vector<string>&res)
-{
-    if(open==0 && close==0)
+    void generate(int open ,int close , string tmp ,vector<string>&res)
     {
-       res.push_back(s);
-        return ;
+        if(open ==0 && close==0){
+            res.push_back(tmp);
+            return;
+        }
+
+        if(open > 0){
+            generate(open-1,close,tmp+'(',res);
+        }
+
+        if(close > open){
+            generate(open,close-1,tmp+')',res);
+        }
     }
-    
-    if(open >0)
-    {
-          generate_pranthese(open-1,close,s+'(',res);
-    }
-    
-    if(close > open)
-    {
-          generate_pranthese(open,close-1,s+')',res);
-    }
-}
     vector<string> generateParenthesis(int n) {
-         vector<string>res;
-     generate_pranthese(n,n,"",res);
-     return res;
+        vector<string> res;
+        
+        generate(n,n,"",res);
+        return res;
     }
 };
