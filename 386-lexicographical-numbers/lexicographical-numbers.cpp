1ed36cc02adcc92a9b817b8dd014033ca06e00ba
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-06-08 12:22:57 +0530

    Time: 75 ms (13.65%) | Memory: 28 MB (11.28%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-        vector<string>st;
+        
+        vector<string>tmp;
         for(int i=1;i<=n;i++)
         {
-            st.push_back(to_string(i));
-        }
-        sort(st.begin(),st.end());
-        vector<int>ans(n);
-        for(int i=0;i<n;i++)
-        {
-            ans[i]=stoi(st[i]);
+            tmp.push_back(to_string(i));
         }
 
+        sort(tmp.begin(),tmp.end());
+
+        vector<int>ans;
+        for(int i=0;i<tmp.size();i++)
+        {
+            ans.push_back(stoi(tmp[i]));
+        }
         return ans;
     }
 };
