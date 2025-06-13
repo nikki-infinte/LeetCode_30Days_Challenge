f8ebefa27c44c7456985b64d6c1d1474df213954
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-06-13 20:13:27 +0530

    Time: 20 ms (40.32%) | Memory: 42.4 MB (77.96%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-        
-    int n=nums.size();
-    if(n<=4)
-    {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        if(n<=4)
         return 0;
+        int start = 0, end = nums[n-1]-nums[0];
+        int ans = end;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(check(nums, mid, n)){
+                ans = mid;
+                end = mid -1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return ans;
     }
-    sort(begin(nums),end(nums));
 
-    int res=INT_MAX;
-    res=min(res,nums[n-4] - nums[0]);
-    res=min(res,nums[n-1]-nums[3]);
-    res=min(res,nums[n-3]-nums[1]);
-     res=min(res,nums[n-2]-nums[2]);
-
-    return res;
+    //sliding window of size n-3
+    bool check(vector<int> &nums, int limit, int n) {
+        if(nums[n-4]-nums[0]<=limit)
+        return true;
+        if(nums[n-3]-nums[1]<=limit)
+        return true;
+        if(nums[n-2]-nums[2]<=limit)
+        return true;
+        if(nums[n-1]-nums[3]<=limit)
+        return true;
+        return false;
     }
 };
