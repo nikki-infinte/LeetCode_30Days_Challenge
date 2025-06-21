9249b4891d2903bbaca56f3fea1e3637dde7f7b3
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-06-21 08:50:48 +0530

    Time: 250 ms (81.65%) | Memory: 281.5 MB (86.39%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        
-        
-        unordered_map<int,vector<int>> graph; 
-        for(auto e : edges) {
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
+    bool validPath(int n, vector<vector<int>>& edges, int source,
+                   int destination) {
+
+        // Step 1: Build adjacency list
+        vector<vector<int>> adj(n);
+        for (auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]); // undirected
         }
-        vector<bool> visited(n,0);        
+
+        vector<bool> visited(n, false);
         queue<int> q;
         q.push(source);
-        visited[source] = 1; 
-        while(!q.empty()) {
-            int curr = q.front();
+        visited[source] = true;
+        while (!q.empty()) {
+            int x = q.front();
             q.pop();
-            if(curr == destination)
-                return 1; 
-            for(auto &node : graph[curr]){
-                if(!visited[node]){
-                    visited[node] = 1; 
-                    q.push(node);
+
+            if (x == destination)
+                return true;
+            for (int neighbor : adj[x]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
                 }
             }
         }
-        
+
         return false;
     }
-        
 };
