eaae64a58021f867755fc1e66e86e2bb0cb14e9f
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-07-01 16:53:43 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (57.52%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k)
-    {
-       /* previous submisson 
-        int n=nums.size();
-        vector<int>temp(n);
-        for(int i=0;i<n;i++)
-        {
-            temp[(i+k)%n]=nums[i];
-        }
-        nums=temp;
-        */
-/*
-        better approach:
-        int n=nums.size();
-        while(k!=0)
-        {
 
-            int temp=nums[n-1];
-            for(int i=0;i<n;i++)
-            {
-                nums[i+1]=nums[i];
-            }
+    void reverse(vector<int>&nums,int i,int j){
+        while(i < j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
         }
-        */
-        //reversal approach
-         k = k % nums.size();
-        reverse(nums,0,nums.size()-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,nums.size()-1);
-
 
     }
-    void reverse(vector<int>& nums,int s,int e)
-    {
-        while(s<=e)
-        {
-            int tmp=nums[s];
-            nums[s]=nums[e];
-            nums[e]=tmp;
-            s++;
-            e--;
+    void rotate(vector<int>& nums, int k) {
+        
+        //reversal algorithm 
+        int n = nums.size();
+        
+        k%=n;
 
-        }
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-k-1);
+        reverse(nums,0,n-1);
+        
     }
 };
