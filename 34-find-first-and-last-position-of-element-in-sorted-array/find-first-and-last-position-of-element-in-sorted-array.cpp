17d6d3131a6be3d0c28bf0e9d1d6f7e953b3dbc0
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-07-03 08:44:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (52.52%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,13 @@
 class Solution {
 public:
-     int ceil_value(vector<int>& arr, int target) {
-        int low = 0, high = arr.size() - 1;
-        int ceil = -1;  // Initialize to -1 for invalid case
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] >= target) {
-                ceil = mid;
-                high = mid - 1;  // Search in the left half
-            } else {
-                low = mid + 1;  // Search in the right half
-            }
-        }
-        return ceil;
-    }
-
-    int floor_value(vector<int>& arr, int target) {
-        int low = 0, high = arr.size() - 1;
-        int floor = -1;  // Initialize to -1 for invalid case
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] <= target) {
-                floor = mid;
-                low = mid + 1;  // Search in the right half
-            } else {
-                high = mid - 1;  // Search in the left half
-            }
-        }
-        return floor;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int f=floor_value(nums,target);
-        int c=ceil_value(nums,target);
-          if (f == -1 || c == -1 || nums[f] != target || nums[c] != target) {
-            return {-1, -1};  // Target not found
-        }
-        return {c,f};
+        int first = lower_bound(nums.begin() , nums.end(),target)-nums.begin();
+        int second = upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1;
+        if (first == nums.size() || nums[first] != target) {
+        return {-1, -1};
+    }
+ 
+        
+        return {first,second};
     }
 };
