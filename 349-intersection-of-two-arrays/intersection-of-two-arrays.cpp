2c586909cd38b589d843b2736910d190238fb2ce
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-07-04 11:09:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (91.12%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -12,19 +12,28 @@ public:
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         vector<int>ans;
-        while(i<n && j<m)
-        {
-            if(nums1[i]==nums2[j] ){
-                 if(ans.empty() || ans.back() != nums1[i]) {
-                    ans.push_back(nums1[i]);
+        // while(i<n && j<m)
+        // {
+        //     if(nums1[i]==nums2[j] ){
+        //          if(ans.empty() || ans.back() != nums1[i]) {
+        //             ans.push_back(nums1[i]);
+        //         }
+        //         i++;
+        //         j++;
+        //     }else if(nums1[i] > nums2[j]){
+        //             j++;   // move forward in nums2
+        //         }else {
+        //             i++;   // move forward in nums1
+        //         }
+        // }
+        // return ans;
+
+        for(int i=0;i<m;i++){
+            if(binary_search(nums1.begin(),nums1.end(),nums2[i])){
+                if( (!ans.empty() && ans.back()!= nums2[i]) || ans.empty()){
+                    ans.push_back(nums2[i]);
                 }
-                i++;
-                j++;
-            }else if(nums1[i] > nums2[j]){
-                    j++;   // move forward in nums2
-                }else {
-                    i++;   // move forward in nums1
-                }
+            }
         }
         return ans;
     }
