18c8abefe2bed10d485a5dcaa28c575bd0461f7b
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-07-04 17:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (73.06%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -10,41 +10,47 @@
  */
 class Solution {
 public:
+    ListNode* reverseNode(ListNode* l1)
+    {
+        ListNode* curr = l1;
+        ListNode* pre = nullptr;
+       
+
+        while(curr!=nullptr){
+           ListNode* nxt = curr->next;
+            curr->next = pre;
+            pre = curr;
+            curr =nxt;
+        }
+        return pre;
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *l3 = new ListNode(0); 
-        ListNode *head=l3;
-        int carry=0;
-        while(l1 && l2)
-        {
-            int sum=l1->val + l2->val+carry;
-             carry=sum/10;
-            l3->next=new ListNode(sum%10);
-            l3=l3->next;
+        
+    //    l1 = reverseNode(l1);
+    //    l2 = reverseNode(l2);
+
+       int carry=0;
+       ListNode* newtmp=new ListNode(-1);
+       ListNode* ans = newtmp;
+       while(l1 != nullptr || l2 != nullptr||carry )
+       {
+        int sum = carry;
+        if(l1!=nullptr){
+            sum+=l1->val;
+            l1=l1->next;
+        }
+
+        if(l2 != nullptr){
+            sum+=l2->val;
             l2=l2->next;
-            l1=l1->next;
         }
+        newtmp->next = new ListNode(sum%10);
+        newtmp = newtmp->next;
+        carry = sum/10;
+       }
 
-        while(l1)
-        {
-            int sum=l1->val+carry;
-             carry=sum/10;
-            l3->next=new ListNode(sum%10);
-            l3=l3->next;
-            l1=l1->next;
-        }
-        while(l2)
-        {
-            int sum=l2->val+carry;
-            carry=sum/10;
-            l3->next=new ListNode(sum%10);
-            l2=l2->next;     
-            l3=l3->next;
-
-        }
-        if(carry)
-        {
-            l3->next=new ListNode(carry);
-        }
-        return head->next;
+     
+       // ListNode* res = reverseNode(ans->next);
+        return ans->next;
     }
 };
