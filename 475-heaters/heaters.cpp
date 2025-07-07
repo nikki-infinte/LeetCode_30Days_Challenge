72b61223b45bae1a6bbcdfade31e3b77acfa9b3f
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-07-07 09:40:36 +0530

    Time: 31 ms (22.49%) | Memory: 28.8 MB (98.92%) - LeetSync

--- 475-heaters/heaters.cpp
+++ 475-heaters/heaters.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
-    int findRadius(vector<int>& houses, vector<int>& heaters) {
-        int max_di=0;
-        for(int i=0;i<houses.size();i++){
-            int min_di = INT_MAX;
-            for(int j=0;j<heaters.size();j++){
-                 min_di = min(abs(heaters[j] - houses[i]), min_di);
+
+    bool coverAll(vector<int>&houses,vector<int>& heaters,int mid){
+         int j=0;
+        for(int i=0;i<houses.size();i++)
+        {
+           
+            while(j<heaters.size() && heaters[j] + mid < houses[i]){
+                j++;
             }
-            max_di =max(max_di,min_di);
+           if(j == heaters.size() || heaters[j] - mid > houses[i]) return false;
+
         }
-        return max_di;
+        return true;
+    }
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        
+        sort(houses.begin(),houses.end());
+        sort(heaters.begin(),heaters.end());
+        int low = 0, high =1e9;
+        int ans = 0;
+        while(low<=high){
+            int mid = (low + high) /2;
+
+            if(coverAll(houses,heaters,mid)){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
     }
 };
