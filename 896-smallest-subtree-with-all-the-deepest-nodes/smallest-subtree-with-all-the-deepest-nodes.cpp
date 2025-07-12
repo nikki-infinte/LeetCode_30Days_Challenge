593a8ca3794d457821f10ef40ede98dcb916f9e5
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-07-12 11:11:47 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (23.23%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+ map<int,vector<TreeNode*>>mp;
+   int levelDepth(TreeNode* root){
+        if(root==nullptr)return 0 ;
+
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        int maxd=0;
+        
+        while(!q.empty()){
+            
+                auto [node,d] = q.front();
+                q.pop();
+
+                
+                maxd = max(d,maxd);
+
+                mp[d].push_back(node);
+                if(node->left){
+                    q.push({node->left , d+1} );
+                }
+                if(node->right){
+                    q.push({node->right,d+1});
+                }
+            
+        }
+    return maxd;
+    }
+
+
+    TreeNode* LCA(TreeNode* root,TreeNode* p,TreeNode* q){
+        if(root==nullptr || root == p || root == q)return root;
+
+        TreeNode* lt = LCA(root->left,p,q);
+        TreeNode* rt = LCA(root->right,p,q);
+
+        if(lt == nullptr && rt == nullptr)return nullptr;
+        else if( lt == nullptr)return rt;
+        else if(rt == nullptr) return lt;
+        else return root;
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        mp.clear();
+        int maxd = levelDepth(root);
+        vector<TreeNode*>deepest = mp[maxd];
+        TreeNode* lca = deepest[0];
+        for(int i=1;i<deepest.size();i++){
+            lca = LCA(root,lca,deepest[i]);
+        }
+        return lca;
+    }
+};
