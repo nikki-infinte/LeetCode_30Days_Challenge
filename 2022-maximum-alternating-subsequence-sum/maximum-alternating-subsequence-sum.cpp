691822a8b64ad79f6ba3d0f4d3270bd29050cce2
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-07-22 08:50:59 +0530

    Time: 277 ms (32.24%) | Memory: 157 MB (47.85%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,29 +1,14 @@
- const int MAXN =100005;
 class Solution {
 public:
-  
-    long long solve(int indx, bool isEven, vector<int>& nums,long long dp[MAXN][2]) {
-        // base
-        if (indx >= nums.size()) {
-            return 0;
-        }
-        if (dp[indx][isEven] != -1) {
-            return dp[indx][isEven];
-        }
-
-        long long skip = solve(indx + 1, isEven, nums,dp);
-        long long val = nums[indx];
-
-        if (!isEven) {
-            val = (-val);
-        }
-        long long take = solve(indx + 1, !isEven, nums,dp) + val;
-        return dp[indx][isEven] = max(take, skip);
-    }
     long long maxAlternatingSum(vector<int>& nums) {
-       int n= nums.size();
-        long long dp[MAXN][2];
-      memset(dp, -1, sizeof(dp));
-        return solve(0, true, nums,dp);
+        int n = nums.size();
+
+        vector<vector<long>>dp(n+1,vector<long>(2,0));
+
+        for(int i=1;i<n+1;i++){
+            dp[i][0] = max(dp[i-1][1]-nums[i-1] , dp[i-1][0]);
+            dp[i][1] = max(dp[i-1][0]+ nums[i-1] , dp[i-1][1]);
+        }
+        return max(dp[n][0],dp[n][1]);
     }
 };
