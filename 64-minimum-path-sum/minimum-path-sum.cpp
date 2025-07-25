9254710300ef0d5d941dac8704ff2e0c591666c4
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-07-25 13:55:52 +0530

    Time: 3 ms (31.97%) | Memory: 16.5 MB (14.55%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> cost(n, vector<int>(m, INT_MAX));  // Track min cost
-        queue<pair<int, int>> q;
-        
-        q.push({0, 0});
-        cost[0][0] = grid[0][0];
-
-        vector<int> x_dir = {1, 0};  // Move only down and right
-        vector<int> y_dir = {0, 1};
-
-        while (!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-
-            for (int i = 0; i < 2; i++) {
-                int new_x = x + x_dir[i];
-                int new_y = y + y_dir[i];
-
-                if (new_x >= 0 && new_y >= 0 && new_x < n && new_y < m) {
-                    int new_cost = cost[x][y] + grid[new_x][new_y];
-
-                    if (new_cost < cost[new_x][new_y]) {  // Only update if new path is better
-                        cost[new_x][new_y] = new_cost;
-                        q.push({new_x, new_y});
-                    }
-                }
-            }
+    int n , m;
+    int solve(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp){
+        if( i >=n || j >=m)return INT_MAX;
+        if( i== n-1 && j==m-1){
+            return dp[i][j]=grid[i][j];
         }
+        if(dp[i][j] != -1)return dp[i][j]; 
+        int takeRight = solve(i,j+1,grid,dp);
+        int takeDown = solve(i+1,j,grid,dp);
+        return dp[i][j] = grid[i][j]+min(takeRight,takeDown);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
 
-        return cost[n-1][m-1];  // Minimum path sum to bottom-right corner
+        return solve(0,0,grid,dp);
+
     }
 };
