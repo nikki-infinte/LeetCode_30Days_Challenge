7978b2c657bf7bf77b5455aa1adc77714d50b7e4
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-04 21:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (70.60%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        if(nums.size()==1)return 1;
+        if (nums.size() == 1)
+            return 1;
 
-        int p_diff = nums[1]-nums[0];
-        int counter = (p_diff != 0)?2:1;
-
-        for(int i=2;i<nums.size();i++)
-        {
-            int curr_d = nums[i] - nums[i-1];
-
-            if(curr_d > 0 && p_diff <=0 || ( curr_d < 0 && p_diff >=0)){
-                counter++;
-                p_diff = curr_d;
+        int preD = nums[1] - nums[0];
+        int len = (preD != 0) ? 2 : 1;
+        for (int i = 2; i < nums.size(); i++) {
+            int curr = nums[i]-nums[i-1];
+            if ((preD <= 0 && curr > 0) || (preD >= 0 && curr < 0)) {
+                len++;
+                preD = curr;
             }
         }
-        return counter;
+        return len;
     }
 };
