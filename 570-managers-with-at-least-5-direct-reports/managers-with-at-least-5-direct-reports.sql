ba13be5401ef6f90853e0e06f68351b152d88a5a
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-09 08:01:06 +0530

    Time: 321 ms (85.55%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,18 @@
-# Write your MySQL query statement below
-Select e.name
+# Using Subquery , 
+-- Select e.name
+-- from Employee e
+-- where (
+--     select count(*)
+--     from Employee e2
+--     where e2.managerId = e.id
+-- ) >= 5 ;
+
+
+# now by self join
+
+select  e.name
 from Employee e
-where (
-    select count(*)
-    from Employee e2
-    where e2.managerId = e.id
-) >= 5 ;
+join Employee e2
+on e.id = e2.managerId
+group by e.id 
+having count(*) >=5;
