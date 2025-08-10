194e7b11b92f4057b33e295d01f6f0546c24c466
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-10 13:01:09 +0530

    Time: 3 ms (89.57%) | Memory: 23.9 MB (57.07%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,13 +3,14 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
 
+        int n =intervals.size();
+
         int start = intervals[0][0];
         int end = intervals[0][1];
-
-        int n = intervals.size();
         vector<vector<int>>ans;
 
-        for(int i=1;i<n;i++){
+        for(int i=1; i<intervals.size() ; i++){
+            
             if(intervals[i][0] <= end){
                 end = max(intervals[i][1],end);
             }else{
@@ -18,7 +19,9 @@ public:
                 end = intervals[i][1];
             }
         }
+
         ans.push_back({start,end});
         return ans;
     }
+
 };
