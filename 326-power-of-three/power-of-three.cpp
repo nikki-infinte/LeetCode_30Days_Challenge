0cf2ca54a1ab50aaf3ba2476352ad7fc98a130ba
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-13 20:18:46 +0530

    Time: 4 ms (47.44%) | Memory: 9.2 MB (5.34%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        if(n==0 || n <0)return false;
+        double p = log10(n) / log10(3);
+
+        if( p - (int)p == 0 ){
+            return true;
+        }else{
+            return false;
+        }
+       
+    }
+};
