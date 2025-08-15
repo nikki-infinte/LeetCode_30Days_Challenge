729974a837d30b39c9c4347e336de6b5cac65439
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-15 21:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.50%) - LeetSync

--- 342-power-of-four/power-of-four.c
+++ 342-power-of-four/power-of-four.c
@@ -1,9 +1,9 @@
 bool isPowerOfFour(int n) {
     if( n == 0)return false;
 
+    if( n == 1)return true;
+    if( n % 4 ==0 ) return isPowerOfFour( n/4);
+    else return false;
+
     
-    while(n!=0 &&  n % 4 == 0){
-        n/=4;
-    }
-    return n ==1  ;
 }
