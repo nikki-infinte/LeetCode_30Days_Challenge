de698ed30751a27850448973b68b8df058eec4e0
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-22 10:48:38 +0530

    Time: 57 ms (69.28%) | Memory: 75.9 MB (58.80%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
-
-    void merged(vector<int>&nums,int low , int high ,int mid){
-
-        vector<int>temp;
-        int left = low , right = mid+1;
-
-         while (left <= mid && right <= high) {
-        if (nums[left] <= nums[right]) temp.push_back(nums[left++]);
-        else temp.push_back(nums[right++]);
-    }
-
-    while (left <= mid) temp.push_back(nums[left++]);
-    while (right <= high) temp.push_back(nums[right++]);
-
-    for (int i = low; i <= high; ++i) {
-        nums[i] = temp[i - low];
-    }
-
-
-    }
-    void merge(vector<int>&nums,int low,int high){
-
-        if(low >= high)return ;
-        int mid = (low + high)/2;
-        merge(nums,low,mid);
-        merge(nums,mid+1,high);
-
-        merged(nums,low,high,mid);
-      
-    }
     vector<int> sortArray(vector<int>& nums) {
-        merge(nums,0,nums.size()-1);
+        priority_queue<int>pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+        for(int i=nums.size()-1;i>=0;i--){
+            nums[i] = pq.top();
+            pq.pop();
+        }
         return nums;
     }
 };
