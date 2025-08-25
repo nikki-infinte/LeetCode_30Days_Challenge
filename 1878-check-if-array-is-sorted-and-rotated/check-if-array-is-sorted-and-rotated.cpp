66787b78bee753856788f1fc05fd252d686f1229
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-25 09:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (45.37%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,46 +1,19 @@
 class Solution {
 public:
-
-    bool isSorted(vector<int>nums){
-        int n = nums.size();
-        int i=0;
-
-        while( i+1 < n){
-            if(nums[i] > nums[i+1])return false;
-            i++;
-        }
-        return true;
-    }
-    void reverse(vector<int>&nums,int i,int j){
-        while(i<j){
-            swap(nums[i],nums[j]);
-            i++;
-            j--;
-        }
-
-    }
-    vector<int>Rotate(int k,vector<int>nums){
-
-        int n = nums.size();
-        k%=n;
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
-        reverse(nums,0,n-1);
-
-        return nums;
-    }
     bool check(vector<int>& nums) {
         
-        if(isSorted(nums))return true;
-        for(int i=1;i<nums.size();i++){
-            vector<int>tmp = Rotate(i,nums);
-            for(int i=0;i<tmp.size();i++){
-                cout<<tmp[i]<<" ";
-            }
-            
-            if(isSorted(tmp))return true;
+        int i=0,n = nums.size();
+        while( i+1 < n){
+            if(nums[i] <= nums[i+1])i++;
+            else break;
         }
-        return false;
-        
+       
+       int peak = i;
+
+       for(int j=peak+1;j<n;j++){
+        if(nums[j] > nums[0] || nums[j] > nums[peak])return false;
+        if(j+1<n && nums[j] > nums[j+1])return false;
+       }
+       return true;
     }
 };
