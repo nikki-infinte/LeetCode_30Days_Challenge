2d12eca213482989630c41bc6a915923c0c924e1
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-28 13:36:30 +0530

    Time: 3 ms (94.47%) | Memory: 40.7 MB (97.15%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/maximum-binary-tree.cpp
@@ -11,32 +11,26 @@
  */
 class Solution {
 public:
-int MaximumIndexVal(vector<int>array,int i,int j){
-    int maxElm = array[i] , maxIndx =i;
-    for(int k = i;k<=j;k++){
-        if(maxElm < array[k]){
-            maxElm = array[k];
-            maxIndx = k;
-        }
-    }
-    return maxIndx;
-}
-
-    TreeNode* solve(int low ,int high,vector<int>&nums){
-
-       if (low > high) return nullptr;
-
-
-
-        int indx = MaximumIndexVal(nums,low,high);
-        TreeNode* node = new TreeNode(nums[indx]);
-
-        node->left = solve(low,indx-1,nums);
-        node->right = solve(indx+1,high,nums);
-        return node;
-        
-    }
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-       return  solve(0,nums.size()-1,nums);
+        stack<TreeNode*>st;
+
+        for(auto n:nums){
+            TreeNode* node = new TreeNode(n);
+            while(!st.empty() && st.top()->val < n  ){
+                node->left = st.top();
+                st.pop();
+            }
+
+
+            if(!st.empty())
+            {
+                st.top()->right = node;
+            }
+
+            st.push(node);
+        }
+         // Root is at bottom of stack
+        while (st.size() > 1) st.pop();
+        return st.top();
     }
 };
