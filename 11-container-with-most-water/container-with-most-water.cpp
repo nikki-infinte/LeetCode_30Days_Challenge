bd872ca5d469c664f13e46a554cf8a8aa6305842
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-28 20:28:14 +0530

    Time: 4 ms (21.84%) | Memory: 62.9 MB (78.65%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,38 +1,21 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <iostream>
-#include <math.h>
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
-     int left =0;
-     int right=height.size()-1;
-     int maxwater=0;
+        int mxArea = INT_MIN;
 
-     while(left <=right)
-     {
-        int width=right-left;
-        int currheight=min(height[left],height[right]);
-        int curr_area=width*currheight;
+        int left = 0,right = height.size()-1;
 
-        maxwater=max(maxwater,curr_area);
+        while(left <= right){
 
-        if(height[left] < height[right])
-        {
-            left+=1;
-        }else{
-            right-=1;
+            int area = (right - left)*min(height[left],height[right]);
+            mxArea = max(mxArea,area);
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+
         }
-     }
-    return maxwater;
-        
+        return mxArea;
     }
 };
