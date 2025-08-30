25e595d8c551e821918b207857d5e75191ae8c0f
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-30 12:47:23 +0530

    Time: 158 ms (2.01%) | Memory: 64.2 MB (7.04%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        vector<int> ans;
-        unordered_map<int, int> umap;
-        
-        // Count occurrences of elements in arr1
-        for (int num : arr1) {
-            umap[num]++;
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr2.size();i++){
+            mp[arr2[i]] =i;
         }
+        sort(arr1.begin(),arr1.end(),[mp]( int a ,  int b){
+            int rankA = mp.count(a) ? mp.at(a):1000 +a;
+            int rankB =mp.count(b)?mp.at(b):1000+b;
 
-        // Add elements that are in arr2 in the correct order
-        for (int num : arr2) {
-            while (umap[num] > 0) {
-                ans.push_back(num);
-                umap[num]--;
-            }
-            umap.erase(num); // Remove processed element from map
-        }
-
-        // Store remaining elements that were not in arr2
-        vector<int> remaining;
-        for (auto& [num, count] : umap) {
-            while (count-- > 0) {
-                remaining.push_back(num);
-            }
-        }
-
-        // Sort the remaining elements
-        sort(remaining.begin(), remaining.end());
-
-        // Append remaining elements to ans
-        ans.insert(ans.end(), remaining.begin(), remaining.end());
-
-        return ans;
+            return rankA < rankB ;
+        });
+        return arr1;
     }
 };
