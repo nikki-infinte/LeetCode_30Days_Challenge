c147cb8993875c1d3e45f94bf0d7b5dc22d1bff7
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-08-30 18:21:52 +0530

    Time: 1111 ms (26.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,5 @@
+select r.contest_id,
+round(count(r.user_id)* 100 / (select count(user_id) from Users ),2 )as percentage
+from register r
+group by r.contest_id
+order by  percentage desc,contest_id ;
