fa7b82637c5887f36d03f20afdab6f50cf518d82
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-09-04 09:59:18 +0530

    Time: 0 ms (100.00%) | Memory: 117.9 MB (93.81%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,27 +10,50 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        //using stack
 
-        stack<int>st;
-        ListNode* tmp=head;
-        while(tmp!=NULL)
-        {
-            st.push(tmp->val);
-            tmp=tmp->next;
+
+    ListNode* reverse(ListNode* root){
+
+        ListNode* curr = root;
+        ListNode* prev = nullptr;
+        while(curr != nullptr){
+            ListNode* nxx = curr->next;
+            curr->next = prev;
+            prev= curr;
+            curr = nxx;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        
+
+        if(head==nullptr || head->next == nullptr)return true;
+        //approach 1 
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow ->next;
+            fast = fast ->next->next;
         }
 
-        ListNode* tmp1=head;
-        while( tmp1->next!=NULL)
-        {
-            if(tmp1->val!=st.top())
-            {
-                return false;
-            }
-            st.pop();
-            tmp1=tmp1->next;
+        ListNode* tmp1 = head;
+        ListNode* tmp2;
+        if(fast == nullptr){
+               tmp2 = reverse(slow); 
+        }else{
+         tmp2 =reverse(slow->next);
+        }
+
+        while(tmp2!=nullptr){
+            if(tmp1->val != tmp2->val)return false;
+            tmp1  = tmp1->next;
+            tmp2 = tmp2 ->next;
         }
         return true;
+
+
+
+
     }
 };
