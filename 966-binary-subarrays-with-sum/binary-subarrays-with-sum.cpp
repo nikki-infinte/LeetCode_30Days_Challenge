ea193f766cc8e4754f78707ad4ddcd4a4ec17fac
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-09-11 17:53:40 +0530

    Time: 13 ms (35.63%) | Memory: 38.5 MB (32.35%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-
-    int atMost(vector<int>& nums, int goal)
-    {
-          int cnt=0;
-        int i=0;
-        int j=0;
-        int n=nums.size();
-        int sum=0;
-        while ( j < n)
-        {
-            if( goal <0)return 0;
-            sum+=nums[j];
-            while( sum > goal && i<=j)
-            {
-                sum -=nums[i];
-                i++;
-            }
-            cnt=cnt+(j-i+1);
-            j++;
-        }
-        return cnt;
-    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         
-        //     int cnt=0;
-        // for(int i=0;i<nums.size() ;i++)
-        // {   int sum=0;
-        //     for(int  j=i;j<nums.size();j++)
-        //     {
-        //         sum+=nums[j];
-        //         if(sum == goal )
-        //         {
-        //             cnt++;
-        //         }else if(sum > goal ){
-        //             continue;
-        //         }
-        //     }
-        // }
-        // return cnt;
+        unordered_map<int,int>umap;
+        int sum =0;
+        int n = nums.size();
+        umap[0]=1;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
 
-        return atMost(nums , goal)-atMost(nums,goal-1);
-      
+            if(umap.find(sum-goal)!=umap.end()){
+                count+=umap[sum-goal];
+            }
+            umap[sum]++;
+        }
+        return count;
+
+
+
+     
     }
 };
