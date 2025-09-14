d700cf5b2353df1b806add3829426e9a3d294bd8
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-09-14 10:24:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (5.41%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,51 +11,47 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        ListNode* newNodeHead = new ListNode(-1);
+        ListNode* newNode = newNodeHead;
+        ListNode* tmp = list1;
+        ListNode* tmp2 = list2;
 
-        if(list1==NULL && list2==NULL)
-        {
-            return list1;
-        }
-        ListNode* newNode=new ListNode(-1);
-        ListNode* head=newNode;
-        ListNode* tmp1=list1;
-        ListNode* tmp2=list2;
-        while(tmp1 !=NULL && tmp2!=NULL)
-        {
-            if(tmp1->val  < tmp2->val)
-            {
-                newNode->next=new ListNode(tmp1->val);
-                tmp1=tmp1->next;
-                newNode=newNode->next;
-            }else if(tmp2->val < tmp1 ->val)
-            {
-                newNode->next=new ListNode(tmp2->val);
-                 tmp2=tmp2->next;
-                newNode=newNode->next;
-            }else if(tmp2->val == tmp1->val) {
-                newNode->next=new ListNode(tmp1->val);
-                tmp1=tmp1->next;
-                newNode=newNode->next;
-                 newNode->next=new ListNode(tmp2->val);
-                 tmp2=tmp2->next;
-                newNode=newNode->next;
+        while(tmp && tmp2){
+
+
+            if(tmp->val == tmp2->val){
+                newNode->next = new ListNode(tmp->val);
+                newNode = newNode->next;
+                newNode->next = new ListNode(tmp2->val);
+
+                tmp=tmp->next;
+                tmp2 = tmp2->next;
+            }else if(tmp->val < tmp2->val){
+
+
+                newNode->next = new ListNode(tmp->val);
+                tmp=tmp->next;
+                
+            }else{
+                newNode->next = new ListNode(tmp2->val);
+                tmp2=tmp2->next;
+               
             }
+             newNode = newNode->next;
         }
 
-        while(tmp1!=NULL)
-        {
-             newNode->next=new ListNode(tmp1->val);
-                tmp1=tmp1->next;
-                newNode=newNode->next;
+        while(tmp){
+            newNode->next = new ListNode(tmp->val);
+            tmp=tmp->next;
+            newNode = newNode->next;
         }
 
-        while(tmp2 !=NULL)
-        {
-             newNode->next=new ListNode(tmp2->val);
-                 tmp2=tmp2->next;
-                newNode=newNode->next;
+        while(tmp2){
+            newNode->next = new ListNode(tmp2->val);
+            tmp2 = tmp2->next;
+            newNode = newNode->next;
         }
-        return head->next;
-    
+        return newNodeHead->next;
     }
 };
