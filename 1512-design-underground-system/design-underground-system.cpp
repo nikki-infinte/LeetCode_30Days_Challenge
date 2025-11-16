ef4b599aba78a175b6731461d50e041a7b3dc938
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-11-16 11:31:47 +0530

    Time: 60 ms (5.17%) | Memory: 72.3 MB (5.88%) - LeetSync

--- 1512-design-underground-system/design-underground-system.cpp
+++ 1512-design-underground-system/design-underground-system.cpp
@@ -1,38 +1,35 @@
 class UndergroundSystem {
 public:
+    map<int,pair<string,int>>checkInMp;
+    map<string,vector<int>>finalMp;
     UndergroundSystem() {
         
     }
-    unordered_map<int,pair<string,int>>checkInMap;
-    unordered_map<string,vector<int>>checks;
     
     void checkIn(int id, string stationName, int t) {
-        checkInMap[id]={stationName,t};
-
-
+        checkInMp[id]={stationName,t};
     }
     
     void checkOut(int id, string stationName, int t) {
-        
-        if(checkInMap.count(id)){
-            string source = checkInMap[id].first;
-            int timeTake = t - checkInMap[id].second;
-            checks[(source+"->"+stationName)].push_back(timeTake);
+        int timetake = t - checkInMp[id].second;
+        string route = checkInMp[id].first + "->"+stationName;
+        finalMp[route].push_back(timetake);
 
-        }
+        
     }
     
- double getAverageTime(string startStation, string endStation) {
-    double avg = 0;
-    string key = startStation + "->" + endStation;
-    if (checks.count(key)) {
-        vector<int> tmp = checks[key];
-        int sum = accumulate(tmp.begin(), tmp.end(), 0);
-        avg = static_cast<double>(sum) / tmp.size();
-    }
-    return avg;
-}
+    double getAverageTime(string startStation, string endStation) {
+        double avg=0;
+        if(finalMp.find(startStation+"->"+endStation)!=finalMp.end()){
+            double sum =0;
 
+            for(auto e:finalMp[startStation+"->"+endStation]){
+                sum+=e;
+            }
+            avg = sum / finalMp[startStation+"->"+endStation].size();
+        }
+        return avg;
+    }
 };
 
 /**
