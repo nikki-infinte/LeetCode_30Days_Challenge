4c26f97a6a1715a0faee11d64ebc0a6d04ded621
Nikita Pawar <139063803+nikki-infinte@users.noreply.github.com>
2025-11-17 13:33:58 +0530

    Time: 3 ms (19.16%) | Memory: 26 MB (79.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-
-    vector<int>leftMax(int n ,vector<int>& height ){
-        vector<int>leftMx(n,0);
-        leftMx[0]= height[0];
-        for(int i=1;i<n;i++)
-        {
-            leftMx[i] = max(leftMx[i-1],height[i]);
-        }
-        return leftMx;
-    }
-
-    vector<int> rightMax(int n,vector<int>& height){
-        
-        vector<int>rightMx(n,0);
-        rightMx[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            rightMx[i] = max(rightMx[i+1],height[i]);
-        }
-        return rightMx;
-    }
     int trap(vector<int>& height) {
         
-        int sum =0;
         int n = height.size();
+        int l=0,r=n-1;
+        int ans =0;
+        int lmax = 0,rmax=0;
 
+        while( l < r){
 
-        vector<int>leftMx=leftMax(n,height);
-        vector<int>rightMx=rightMax(n,height);
+            lmax = max(lmax,height[l]);
+            rmax = max(rmax,height[r]);
 
-
-        for(int i=0;i<height.size();i++)
-        {
-            sum+=min(leftMx[i],rightMx[i])-height[i];
+            if(lmax<rmax){
+                ans += (lmax- height[l]);
+                l++;
+            }else{
+                ans += (rmax-height[r]);
+                r--;
+            }
         }
-        return sum ;
+        return ans;
     }
 };
